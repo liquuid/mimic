bd5c9999540ad9aa364aae28885c686f97d4f647
Fernando Silva <liquuid@gmail.com>
2016-09-14 12:48:56 -0300

    disabling admin

--- mimic/settings.py
+++ mimic/settings.py
@@ -36,7 +36,6 @@ DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
