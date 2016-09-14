ca20ad376677865304f919e2ff35edb7dfe27a69
Fernando Silva <liquuid@gmail.com>
2016-09-14 12:27:29 -0300

    adding STATIC_ROOT

--- mimic/settings.py
+++ mimic/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Email configuration
 EMAIL_BACKEND = config('EMAIL_BACKEND')
